22a47768fce0a45bf13550718913a32e238b2215
Beatriz Murase <beatriz.murase@cobli.co>
2022-04-09 22:54:02 -0300

    add transaction validation method

--- domain/model/transaction.go
+++ domain/model/transaction.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/asaskevich/govalidator"
+
 type Transaction struct {
 	Base              `valid:"required"`
 	AccountFrom       *Account `valid:"-"`
@@ -8,4 +10,14 @@ type Transaction struct {
 	Status            string   `json:"status" valid:"notnull"`
 	Description       string   `json:"description" valid:"notnull"`
 	CancelDescription string   `json:"cancel_description" valid:"-"`
+}
+
+func (transaction *Transaction) isValid() error {
+	_, err := govalidator.ValidateStruct(transaction)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
