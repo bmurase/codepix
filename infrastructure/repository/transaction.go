d82c8ffe6066040bb95ef1eb593b11a294c8a9cb
Beatriz Murase <beatriz.murase@cobli.co>
2022-04-10 14:21:06 -0300

    create transaction repository

---
+++ infrastructure/repository/transaction.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/bmurase/codepix/domain/model"
+	gorm "gorm.io/gorm"
+)
+
+// Register(transaction *Transaction) error
+// 	Save(transaction *Transaction) error
+// 	Find(id string) (*Transaction, error)
+
+type TransactionRepository struct {
+	Db *gorm.DB
+}
+
+func (r *TransactionRepository) Register(transaction *model.Transaction) error {
+	err := r.Db.Create(transaction).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *TransactionRepository) Save(transaction *model.Transaction) error {
+	err := r.Db.Save(transaction).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *TransactionRepository) Find(id string) (*model.Transaction, error) {
+	var transaction model.Transaction
+
+	r.Db.Preload("AccountFrom.Bank").First(&transaction, "id = ?", id)
+
+	if transaction.ID == "" {
+		return nil, fmt.Errorf("no transaction was found with given id")
+	}
+
+	return &transaction, nil
+}
