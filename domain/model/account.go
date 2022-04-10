3f5e19e159035532e762825d12d5eeeacf6e9a8e
Beatriz Murase <beatriz.murase@cobli.co>
2022-04-09 22:06:26 -0300

    add validation method to account model

--- domain/model/account.go
+++ domain/model/account.go
@@ -1,8 +1,20 @@
 package model
 
+import "github.com/asaskevich/govalidator"
+
 type Account struct {
 	Base      `valid:"required"`
 	OwnerName string `json: "owner_name" valid:"notnull"`
 	Bank      *Bank  `valid: "-"`
 	Number    string `json:"number" valid:"notnull"`
+}
+
+func (account *Account) isValid() error {
+	_, err := govalidator.ValidateStruct(account)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
