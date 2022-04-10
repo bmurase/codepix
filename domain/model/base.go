edfa83c7cb6e25798e6e926043975fe387217e2d
Beatriz Murase <beatriz.murase@cobli.co>
2022-04-09 22:03:16 -0300

    add validator to models

--- domain/model/base.go
+++ domain/model/base.go
@@ -2,11 +2,17 @@ package model
 
 import (
 	"time"
+
+	"github.com/asaskevich/govalidator"
 )
 
-type Base struct {
-	ID string `json: "id"`
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
-	CreatedAt time.Time `json: "created_at"`
-	UpdatedAt time.Time `json: "updated_at"`
+type Base struct {
+	ID string `json: "id" valid:"uuid"`
+
+	CreatedAt time.Time `json: "created_at" valid: "-"`
+	UpdatedAt time.Time `json: "updated_at" valid: "-"`
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bmurase/codepix
 
 go 1.15
 
-require github.com/satori/go.uuid v1.2.0
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/satori/go.uuid v1.2.0
+)
