7645a9396db4e77437f14e830fd5f080f182da1a
Beatriz Murase <beatriz.murase@cobli.co>
2022-04-09 22:14:42 -0300

    add pix key validation method

--- domain/model/pixKey.go
+++ domain/model/pixKey.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/asaskevich/govalidator"
+
 type PixKey struct {
 	Base      `valid:"required"`
 	Kind      string   `json:"kind" valid:"notnull"`
@@ -7,4 +9,14 @@ type PixKey struct {
 	AccountID string   `json:"account_id" valid:"notnull"`
 	Account   *Account `valid:"-"`
 	Status    string   `json:"status" valid:"notnull"`
+}
+
+func (pixKey *PixKey) isValid() error {
+	_, err := govalidator.ValidateStruct(pixKey)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
