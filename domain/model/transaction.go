98f43f6f368a8b10c240e548bade90d49c67b21c
Beatriz Murase <beatriz.murase@cobli.co>
2022-04-09 22:52:57 -0300

    create transaction model struct

---
+++ domain/model/transaction.go
@@ -0,0 +1,11 @@
+package model
+
+type Transaction struct {
+	Base              `valid:"required"`
+	AccountFrom       *Account `valid:"-"`
+	Ammount           float64  `json:"amount" valid:"notnull"`
+	PixKeyTo          *PixKey  `valid:"-"`
+	Status            string   `json:"status" valid:"notnull"`
+	Description       string   `json:"description" valid:"notnull"`
+	CancelDescription string   `json:"cancel_description" valid:"-"`
+}
