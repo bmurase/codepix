8cada0136d0143c2038a0dfa7c164d974dd5a5c5
Beatriz Murase <beatriz.murase@cobli.co>
2022-04-16 23:18:02 -0300

    create kafka processor

---
+++ application/kafka/process.go
@@ -0,0 +1,20 @@
+package kafka
+
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/jinzhu/gorm"
+)
+
+type KafkaProcessor struct {
+	Database     *gorm.DB
+	Producer     *ckafka.Producer
+	DeliveryChan chan ckafka.Event
+}
+
+func NewKafkaProcessor(database *gorm.DB, producer *ckafka.Producer, deliveryChan chan ckafka.Event) *KafkaProcessor {
+	return &KafkaProcessor{
+		Database:     database,
+		Producer:     producer,
+		DeliveryChan: deliveryChan,
+	}
+}
